df5c4a49b27e0f26a8c7f5abc4c587c6275df8fb
Abdul Mumit <uc@Zeppelin.local>
2016-08-02 19:50:55 +0600

    Add new install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-# Install brew if not exists
-if ! [ -x "$(command -v brew)" ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [[ "$OSTYPE" == "darwin"* ]]; then # OSX
+    # Install brew if not exists
+    if ! [ -x "$(command -v brew)" ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    if [[ "$(brew list --versions zsh)" == "" ]]; then
+        brew update
+        # Install zsh
+        brew install zsh
+    fi
 fi
 
-if [[ "$(brew list --versions zsh)" == "" ]]; then
-  brew update
-  # Install zsh
-  brew install zsh
-fi
-
-if ! [ -d "$HOME/.oh-my-zsh" ]; then
-  # Install oh-my-zsh
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-if ! [ -f "$HOME/.vim/autoload/plug.vim" ]; then
-  # Install plugin manager for vim
-  curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+# TODO: install zsh for linux
 
 predir=$(pwd)
 cd $(dirname $0)
 dir=$(pwd)
 cd $predir
 
-ln -is $dir/.vimrc "$HOME/.vimrc"
-ln -is $dir/.gvimrc "$HOME/.gvimrc"
-ln -is $dir/.zshrc "$HOME/.zshrc"
-ln -is $dir/.my.cnf "$HOME/.my.cnf"
-ln -is $dir/.gitconfig "$HOME/.gitconfig"
+source $dir/vim/script.sh
+source $dir/zsh/script.sh
+source $dir/git/script.sh
