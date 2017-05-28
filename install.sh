473dd08022d011f70de6480482a38d155be2eec4
Abdul Mumit <mamumit07@gmail.com>
2017-05-28 21:52:54 +0600

    Update for linux

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 if [[ "$OSTYPE" == "darwin"* ]]; then # OSX
-    # Install brew if not exists
-    if ! [ -x "$(command -v brew)" ]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
+  # Install brew if not exists
+  if ! [ -x "$(command -v brew)" ]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-    if [[ "$(brew list --versions zsh)" == "" ]]; then
-        brew update
-        # Install zsh
-        brew install zsh
-    fi
+  brew update
+  # Install minimum env
+  brew install zsh vim git
+
+elif [[ "$OSTYPE" == "linux"* ]]; then # LINUX
+  pacman -S zsh git vim --noconfirm -q
 fi
 
-# TODO: install zsh for linux
+# TODO: install zsh for ubuntu
 
 predir=$(pwd)
 cd $(dirname $0)
