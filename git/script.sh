7c428c9c7a85bd9f7d8090dea612cede653a0e8f
Abdul Mumit <mamumit07@gmail.com>
2018-10-02 23:22:38 +0200

    Update to new

--- git/script.sh
+++
@@ -1,8 +0,0 @@
-predir=$(pwd)
-cd $(dirname $0)
-dir=$(pwd)
-cd $predir
-
-ln -vis $dir/gitconfig ~/.gitconfig
-mkdir -p ~/.git_template/hooks/
-ln -vis $dir/hooks/* ~/.git_template/hooks/
--- install.sh
+++ install.sh
@@ -1,28 +1,8 @@
 #!/bin/sh
 
-if [[ "$OSTYPE" == "darwin"* ]]; then # OSX
 
-  if ! [ -x "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 1
-  fi
+DIR=`dirname $(realpath $0)`
 
-  # brew update
-  # Install minimum env
-  # brew install zsh vim git tmux
-
-elif [[ "$OSTYPE" == "linux"* ]]; then # LINUX
-
-  sudo pacman -S tmux python zsh git vim --noconfirm -q || exit 1
-
-fi
-
-# TODO: install zsh for ubuntu
-
-predir=$(pwd)
-cd $(dirname $0)
-dir=$(pwd)
-cd $predir
-
-$dir/vim/script.sh || exit 1
-$dir/zsh/script.sh || exit 1
-$dir/git/script.sh || exit 1
+for file in .vimrc .gitconfig .zshrc .tmux.conf; do
+	[ -f $DIR/$file ] && ln -fs $DIR/$file $HOME
+done
